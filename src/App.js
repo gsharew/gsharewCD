42e1d34cd71237feae19a501d5ab390380d38ada
gsharew <gsharewz@gmail.com>
2023-02-10 20:48:47 +0300

    some minor errors are resolved

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         This continious deployment is prepared by  Getachew Sharew Abrha
+         app.icladdis.com continous deployment under preparation
         </a>
       </header>
     </div>
