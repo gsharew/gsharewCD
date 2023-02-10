529d5b9688a4e903f2050ee1db3f78797aafe058
gsharew <gsharewz@gmail.com>
2023-02-10 20:41:28 +0300

    some deperecated files are added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. This is a continous deployment
         </p>
@@ -15,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         This continious deployment is prepared by  Getachew Sharew
+         This continious deployment is prepared by  Getachew Sharew Abrha
         </a>
       </header>
     </div>
